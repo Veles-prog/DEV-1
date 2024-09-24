19f029e9dcbe33069b258a7da9bb45dafb70b7ef
Veles-prog <littlegun670@gmail.com>
2024-09-24 16:29:41 +0500

    DEV-1_COMMIT-2

--- main.py
+++ main.py
@@ -2,28 +2,23 @@ def main(v_result):
     a = v_result.split()
 
     if len(a) != 3:
-        print('throws Exception')
-        return
+        return "throws Exception"
 
     if a[0] not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
-        print('throws Exception')
-        return
+        return "throws Exception"
 
     if a[2] not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
-        print('throws Exception')
-        return
+        return "throws Exception"
 
     if a[1] not in ['+', '-', '/', '*']:
-        print('throws Exception')
-        return
-
+        return "throws Exception"
     if a[1] == '+':
-        print(int(a[0]) + int(a[2]))
+        return(int(a[0]) + int(a[2]))
     elif a[1] == '-':
-        print(int(a[0]) - int(a[2]))
+        return(int(a[0]) - int(a[2]))
     elif a[1] == '*':
-        print(int(a[0]) * int(a[2]))
+        return(int(a[0]) * int(a[2]))
     elif a[1] == '/':
-        print(int(int(a[0]) / int(a[2])))
+        return(int(int(a[0]) // int(a[2])))
 
-main(input())
+print(main(input()))
